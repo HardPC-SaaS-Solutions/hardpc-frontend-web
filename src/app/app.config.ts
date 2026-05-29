ff0e49878d7e60140cd1304469084714617c340d
FabrizioTC06 <fabrizioteodorcelis06@gmail.com>
2026-05-29 04:48:06 -0500

    chore: configuracion de tailwind, primeng y optimizacion de app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    // 1. Optimización de rendimiento (El mesero agrupador)
+    provideZoneChangeDetection({ eventCoalescing: true }),
+
+    // 2. Sistema de rutas para cambiar de pantallas
+    provideRouter(routes),
+
+    // 3. Cliente HTTP nativo para consumir la API de HardPC
+    provideHttpClient(withInterceptors([]))
   ]
 };
