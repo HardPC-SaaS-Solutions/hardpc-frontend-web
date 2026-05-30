f5dab447eeab32ec26c2da21cb5b90ea75ba153b
FabrizioTC06 <fabrizioteodorcelis06@gmail.com>
2026-05-30 04:49:06 -0500

    chore: inicializacion de variables de entorno (apiUrl)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  // URL pública del backend desplegado en la nube (Render o Railway)
+  apiUrl: 'URL_DEL_BACKEND_EN_LA_NUBE'
+};
